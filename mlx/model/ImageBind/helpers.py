84a2369dab3a53929c67635adf1ab306ab3f2f42
Goekdeniz-Guelmez <Gulmezdeniz1999@gmail.com>
2024-05-27 18:49:43 +0200

    update and first architecture

--- mlx/model/ImageBind/helpers.py
+++ mlx/model/ImageBind/helpers.py
@@ -73,3 +73,41 @@ class SelectEOSAndProject(nn.Module):
         # Projektionsschritt
         x = self.proj(x)
         return x
+
+
+class VerboseNNModule(nn.Module):
+    """
+    Wrapper around nn.Module that prints registered buffers and parameter names.
+    """
+
+    @staticmethod
+    def get_readable_tensor_repr(name: str, wieghts: mx.array) -> str:
+        st = (
+            "("
+            + name
+            + "): "
+            + "tensor("
+            + str(tuple(wieghts[1].shape))
+            + ", requires_grad="
+            + str(wieghts[1])
+            + ")\n"
+        )
+        return st
+
+    def extra_repr(self) -> str:
+        named_modules = set()
+        for p in self.named_modules():
+            named_modules.update([p[0]])
+        named_modules = list(named_modules)
+
+        string_repr = ""
+        for p in self.named_parameters():
+            name = p[0].split(".")[0]
+            if name not in named_modules:
+                string_repr += self.get_readable_tensor_repr(name, p)
+
+        for p in self.named_buffers():
+            name = p[0].split(".")[0]
+            string_repr += self.get_readable_tensor_repr(name, p)
+
+        return string_repr
